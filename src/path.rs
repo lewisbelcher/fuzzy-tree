59de40040487fc4bd4dec056907a33c5eb1370d9
Lewis Belcher <belcher_lewis@protonmail.com>
2019-09-20 21:56:54 +0200

    Almost there!..
    Tree printing is almost complete.

--- src/path.rs
+++ src/path.rs
@@ -3,7 +3,7 @@ use std::path;
 
 #[derive(Eq, PartialEq, Debug)]
 pub struct Path {
-	components: Vec<String>,
+	pub components: Vec<String>,
 	pub selected: bool,
 }
 
@@ -40,12 +40,16 @@ impl Path {
 		}
 	}
 
-	pub fn print_joined(&self) {
-		// self.components.join(&path::MAIN_SEPARATOR.to_string())
-		print!(
-			"{} ",
-			self.components.join(&path::MAIN_SEPARATOR.to_string())
-		);
+	pub fn joined(&self) -> String {
+		self.components.join(&path::MAIN_SEPARATOR.to_string())
+	}
+
+	pub fn basename(&self) -> &str {
+		&self.components[self.components.len() - 1]
+	}
+
+	pub fn len(&self) -> usize {
+		self.components.len()
 	}
 }
 
@@ -59,6 +63,12 @@ pub fn create_paths(string: Vec<u8>) -> Vec<Path> {
 		.collect();
 
 	paths.sort();
+
+	// paths.insert(0, Path::new("".to_string()));
+	// for p in &mut paths {
+	// 	p.components.insert(0, ".".to_string());
+	// }
+
 	paths
 }
 
@@ -89,6 +99,68 @@ pub fn filter<'t>(paths: &'t [Path], pattern: &str) -> Vec<usize> {
 		})
 		.collect()
 }
+
+fn is_child_of(p1: &Path, p2: &Path) -> bool {
+	if p2.len() >= p1.len() {
+		return false;
+	}
+	p1.components[..p2.len()] == p2.components[..]
+}
+
+fn is_only_child(p: &Path, paths: &[Path]) -> bool {
+	let len = p.components.len();
+	let reference = &p.components[..len - 1];
+
+	for pth in paths.iter() {
+		if len == pth.components.len() && reference == &pth.components[..len - 1] {
+			return false;
+		}
+	}
+	true
+}
+
+fn in_same_dir(p1: &Path, p2: &Path) -> bool {
+	p1.len() == p2.len() && p1.components[..p1.len() - 1] == p2.components[..p1.len() - 1]
+}
+
+fn create_dir(mut i: usize, prefix: &str, paths: &[Path], lines: &mut Vec<String>) -> usize {
+	if i == paths.len() {
+		return i;
+	}
+
+	let pth1 = &paths[i];
+
+	let extra = if i < paths.len() - 1 && in_same_dir(pth1, &paths[i + 1]) {
+		"├── "
+	} else {
+		"└── "
+	};
+	lines.push(prefix.to_owned() + extra + pth1.basename());
+
+	i += 1;
+	if i == paths.len() {
+		return i;
+	}
+
+	let pth2 = &paths[i];
+
+	if is_child_of(pth2, pth1) {
+		let extra = if i < paths.len() - 1 && is_only_child(pth2, &paths[i + 1..]) {
+			"    "
+		} else {
+			"│   "
+		};
+		return create_dir(i, &(prefix.to_owned() + extra), paths, lines);
+	} else {
+		let diff = prefix.len() - (4 * (pth1.len() - pth2.len()));
+		return create_dir(i, &prefix[..diff], paths, lines);
+	}
+}
+
+/// Create a vector of strings representing lines in the tree printout.
+///
+/// TODO: I think this needs to be a recursive function!
+// pub fn create_tree() {}
 
 #[cfg(test)]
 mod test {
@@ -142,5 +214,55 @@ mod test {
 		assert_eq!(filter(&paths[..], "x y"), vec![]);
 		assert_eq!(filter(&paths[..], "x h"), vec![0]);
 		assert_eq!(filter(&paths[..], "here"), vec![0, 1, 2]);
+	}
+
+	#[test]
+	fn is_child_of_correctness() {
+		let mut p1 = Path::new("A".to_string());
+		let mut p2 = Path::new("B".to_string());
+		assert!(!is_child_of(&p1, &p2));
+
+		p1 = Path::new("src/bayes".to_string());
+		p2 = Path::new("src/bayes/blend.c".to_string());
+		assert!(!is_child_of(&p1, &p2));
+
+		p1 = Path::new("src/bayes".to_string());
+		p2 = Path::new("src/bayes/blend.c".to_string());
+		assert!(is_child_of(&p2, &p1));
+	}
+
+	#[test]
+	fn is_only_child_test() {
+		let p = Path::new("src/bayes".to_string());
+		let mut paths = vec![
+			Path::new("src/bayes/blend.c".to_string()),
+			Path::new("src/bayes/rand.c".to_string()),
+			Path::new("x.txt".to_string()),
+		];
+		assert!(is_only_child(&p, &paths[..]));
+
+		paths = vec![
+			Path::new("src/bayes/blend.c".to_string()),
+			Path::new("src/bayes/rand.c".to_string()),
+			Path::new("src/cakes".to_string()),
+			Path::new("x.txt".to_string()),
+		];
+		assert!(!is_only_child(&p, &paths[..]));
+	}
+
+	#[test]
+	fn create_dir_correct() {
+		let mut lines = Vec::new();
+		let paths = vec![
+			Path::new("A".to_string()),
+			Path::new("B".to_string()),
+			Path::new("src".to_string()),
+			Path::new("src/bayes".to_string()),
+			Path::new("src/bayes/blend.c".to_string()),
+			Path::new("src/bayes/rand.c".to_string()),
+			Path::new("x.txt".to_string()),
+		];
+		let response = create_dir(0, "", &paths[..], &mut lines);
+		println!("{}", lines.join("\n"));
 	}
 }
