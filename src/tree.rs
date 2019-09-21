77e01e1460d9e8bdae61f269b7c7c3e2419ba107
Lewis Belcher <belcher_lewis@protonmail.com>
2019-09-21 11:01:27 +0200

    Implement tree graph data structure

---
+++ src/tree.rs
@@ -0,0 +1,69 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+type XBranch<T> = Rc<RefCell<Branch<T>>>;
+
+pub struct Branch<T> {
+	parent: Option<XBranch<T>>,
+	pub elem: T,
+	children: Option<Vec<XBranch<T>>>,
+}
+
+impl<T> Branch<T> {
+	pub fn new(elem: T) -> Rc<RefCell<Self>> {
+		Rc::new(RefCell::new(Branch {
+			parent: None,
+			elem,
+			children: None,
+		}))
+	}
+
+	pub fn depth(&self) -> usize {
+		1 + match &self.parent {
+			Some(p) => p.borrow().depth(),
+			None => 0,
+		}
+	}
+}
+
+fn add<T>(child: &XBranch<T>, parent: &XBranch<T>) {
+	let mut children = match parent.borrow_mut().children.take() {
+		Some(v) => v,
+		None => Vec::new(),
+	};
+	children.push(Rc::clone(child));
+	parent.borrow_mut().children = Some(children);
+	child.borrow_mut().parent = Some(Rc::clone(parent));
+}
+
+pub trait Breeder<T> {
+	fn add_child(&self, child: &XBranch<T>);
+	fn add_parent(&self, parent: &XBranch<T>);
+}
+
+impl<T> Breeder<T> for XBranch<T> {
+	fn add_child(&self, child: &XBranch<T>) {
+		add(child, self);
+	}
+
+	fn add_parent(&self, parent: &XBranch<T>) {
+		add(self, parent);
+	}
+}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+
+	#[test]
+	fn add_some_depth() {
+		let a = Branch::new("a");
+		let b = Branch::new("b");
+		let c = Branch::new("c");
+		a.add_child(&b);
+		b.add_child(&c);
+		assert_eq!(a.borrow().depth(), 1);
+		assert_eq!(b.borrow().depth(), 2);
+		assert_eq!(c.borrow().depth(), 3);
+	}
+}
