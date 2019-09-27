e4f17a45ea2005d8277a3177bfb4410d2f061f08
Lewis Belcher <belcher_lewis@protonmail.com>
2019-09-28 01:05:27 +0200

    Unify tree and path
    This is required to make `selected` mutable via Rc while immutable
    references are present.

--- src/path.rs
+++ src/path.rs
@@ -1,14 +1,23 @@
-use crate::tree::{self, Breeder};
+use std::cell::RefCell;
 use std::cmp::Ordering;
+use std::fmt;
 use std::path;
 use std::rc::Rc;
 
-pub type PathBranch<'a> = tree::XBranch<&'a Path>;
+pub type RcPath = Rc<RefCell<Path>>;
 
-#[derive(Eq, PartialEq, Debug)]
+#[derive(Eq, PartialEq)]
 pub struct Path {
-	pub components: Vec<String>,
-	pub selected: bool,
+	parent: Option<RcPath>,
+	components: Vec<String>,
+	selected: bool,
+	children: Option<Vec<RcPath>>,
+}
+
+impl fmt::Debug for Path {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		write!(f, "{:?}; Children {:#?}:", self.components, self.children)
+	}
 }
 
 impl Ord for Path {
@@ -34,75 +43,84 @@ impl PartialOrd for Path {
 }
 
 impl Path {
-	pub fn new(string: String) -> Path {
-		Path {
+	pub fn new(string: String) -> RcPath {
+		Rc::new(RefCell::new(Path {
+			parent: None,
 			components: string
 				.split(path::MAIN_SEPARATOR)
 				.map(|x| x.to_string())
 				.collect(),
 			selected: false,
-		}
+			children: None,
+		}))
 	}
 
-	pub fn from(string: &str) -> Path {
+	pub fn from(string: &str) -> RcPath {
 		Path::new(string.to_string())
 	}
+}
 
-	pub fn joined(&self) -> String {
-		self.components.join(&path::MAIN_SEPARATOR.to_string())
+fn add(child: &RcPath, parent: &RcPath) {
+	let mut children = match parent.borrow_mut().children.take() {
+		Some(v) => v,
+		None => Vec::new(),
+	};
+	children.push(Rc::clone(child));
+	parent.borrow_mut().children = Some(children);
+	child.borrow_mut().parent = Some(Rc::clone(parent));
+}
+
+/// Since Paths are wrapped in Rc's which cannot be implemented on directly,
+/// functionality has to be implemented via trait. These could be split into
+/// logical groups of behaviours.
+pub trait PathBehaviour {
+	fn add_child(&self, child: &RcPath);
+	fn add_parent(&self, parent: &RcPath);
+	fn is_child_of(&self, other: &RcPath) -> bool;
+	fn joined(&self) -> String;
+	fn basename(&self) -> &str;
+	fn len(&self) -> usize;
+}
+
+impl PathBehaviour for RcPath {
+	fn add_child(&self, child: &RcPath) {
+		add(child, self);
 	}
 
-	pub fn basename(&self) -> &str {
-		&self.components[self.components.len() - 1]
+	fn add_parent(&self, parent: &RcPath) {
+		add(self, parent);
 	}
 
-	pub fn len(&self) -> usize {
-		self.components.len()
+	fn is_child_of(&self, other: &RcPath) -> bool {
+		if other.len() >= self.len() {
+			return false;
+		}
+		self.borrow().components[..other.len()] == other.borrow().components[..]
 	}
 
-	pub fn is_parent_of(&self, other: &Path) -> bool {
-		is_child_of(other, self)
+	fn joined(&self) -> String {
+		self.borrow()
+			.components
+			.join(&path::MAIN_SEPARATOR.to_string())
 	}
 
-	pub fn is_child_of(&self, other: &Path) -> bool {
-		is_child_of(self, other)
+	fn basename(&self) -> &str {
+		// TODO: See if there is a safe way around this:
+		unsafe { &(*self.as_ptr()).components[self.len() - 1] }
+	}
+
+	fn len(&self) -> usize {
+		self.borrow().components.len()
 	}
 }
 
-fn is_child_of(p1: &Path, p2: &Path) -> bool {
-	if p2.len() >= p1.len() {
-		return false;
-	}
-	p1.components[..p2.len()] == p2.components[..]
-}
-
-/// Create multiple paths from a `find`-like command output.
-pub fn create_paths(string: Vec<u8>) -> Vec<Path> {
-	let mut paths: Vec<Path> = String::from_utf8(string)
-		.unwrap()
-		.split('\n')
-		.filter(|x| !x.is_empty())
-		.map(|x| Path::new(x.to_string()))
-		.collect();
-
-	paths.sort();
-
-	// Add CWD as "."
-	paths.insert(0, Path::new(".".to_string()));
-	for p in &mut paths {
-		p.components.insert(0, ".".to_string());
-	}
-
-	paths
-}
-
-fn _match(pth: &Path, pattern: &str) -> bool {
+fn _match(pth: &RcPath, pattern: &str) -> bool {
 	let split: Vec<&str> = pattern.split(" ").filter(|x| !x.is_empty()).collect();
 	let mut matched;
 
 	for s in split {
 		matched = false;
-		for c in &pth.components {
+		for c in &pth.borrow().components {
 			matched |= c.contains(s);
 		}
 		if !matched {
@@ -112,7 +130,8 @@ fn _match(pth: &Path, pattern: &str) -> bool {
 	true
 }
 
-pub fn filter<'t>(paths: &'t [Path], pattern: &str) -> Vec<usize> {
+/// Return indices of paths that are selected.
+pub fn filter<'t>(paths: &'t [RcPath], pattern: &str) -> Vec<usize> {
 	let mut i = 0;
 	paths
 		.iter()
@@ -124,11 +143,27 @@ pub fn filter<'t>(paths: &'t [Path], pattern: &str) -> Vec<usize> {
 		.collect()
 }
 
-fn _ischild(a: &PathBranch, b: &PathBranch) -> bool {
-	a.borrow().elem.is_child_of(b.borrow().elem)
+/// Create multiple paths from a `find`-like command output.
+pub fn create_paths(string: Vec<u8>) -> Vec<RcPath> {
+	let mut paths: Vec<RcPath> = String::from_utf8(string)
+		.unwrap()
+		.split('\n')
+		.filter(|x| !x.is_empty())
+		.map(|x| Path::from(x))
+		.collect();
+
+	paths.sort();
+
+	// Add CWD as "."
+	for p in &mut paths {
+		p.borrow_mut().components.insert(0, ".".to_string());
+	}
+	paths.insert(0, Path::from("."));
+
+	paths
 }
 
-fn peek<'a>(stack: &'a [PathBranch<'a>], i: usize) -> Option<&'a PathBranch<'a>> {
+fn peek(stack: &[RcPath], i: usize) -> Option<&RcPath> {
 	if i < stack.len() {
 		return Some(&stack[i]);
 	}
@@ -137,18 +172,14 @@ fn peek<'a>(stack: &'a [PathBranch<'a>], i: usize) -> Option<&'a PathBranch<'a>>
 
 macro_rules! debug_relation {
 	($obj1:expr; child $obj2:expr) => {
-		println!(
-			"{:?} is child of {:?}",
-			$obj1.borrow().elem,
-			$obj2.borrow().elem
-		);
+		println!("{:?} is child of {:?}", $obj1.borrow(), $obj2.borrow());
 	};
 	($obj1:expr; unrelated $obj2:expr, $obj3:expr) => {
 		println!(
 			"{:?} is unrelated to {:?} and {:?}",
-			$obj1.borrow().elem,
-			$obj2.borrow().elem,
-			$obj3.borrow().elem
+			$obj1.borrow(),
+			$obj2.borrow(),
+			$obj3.borrow()
 		);
 	};
 }
@@ -166,9 +197,9 @@ macro_rules! debug_relation {
 ///  3. In all other cases: break with `i` and move up one recursion frame
 fn _create_tree<'a>(
 	mut i: usize,
-	base: &PathBranch<'a>,
-	mut prev: Option<&PathBranch<'a>>,
-	stack: &'a [PathBranch<'a>],
+	base: &RcPath,
+	mut prev: Option<&'a RcPath>,
+	stack: &'a [RcPath],
 ) -> usize {
 	// TODO: Use log package
 	loop {
@@ -176,18 +207,18 @@ fn _create_tree<'a>(
 		i += 1;
 		if let Some(next) = peek(stack, i) {
 			if let Some(prev) = prev {
-				if _ischild(&next, &prev) {
+				if next.is_child_of(&prev) {
 					debug_relation!(next; child base);
 					prev.add_child(&next);
 					i = _create_tree(i, &prev, Some(&next), stack) - 1;
-				} else if _ischild(&next, &base) {
+				} else if next.is_child_of(&base) {
 					debug_relation!(next; child base);
 					base.add_child(&next);
 				} else {
 					debug_relation!(next; unrelated base, prev);
 					break i;
 				}
-			} else if _ischild(&next, &base) {
+			} else if next.is_child_of(&base) {
 				debug_relation!(next; child base);
 				base.add_child(&next);
 			} else {
@@ -206,18 +237,16 @@ fn _create_tree<'a>(
 
 /// Create relationships between all nodes in the directory structure for
 /// `paths`.
-pub fn create_tree<'a>(paths: &'a Vec<PathBranch<'a>>) -> PathBranch<'a> {
+pub fn create_tree(paths: &Vec<RcPath>) -> RcPath {
 	_create_tree(0, &paths[0], None, &paths[..]);
 	Rc::clone(&paths[0])
 }
 
-
-
 /// Inner recursive function to create a string representation of a directory
 /// tree.
-fn _tree_string(root: &PathBranch, lines: &mut Vec<String>, prefix: &str) {
+fn _tree_string(root: &RcPath, lines: &mut Vec<String>, prefix: &str) {
 	if let Some(children) = &root.borrow().children {
-		let children: Vec<&PathBranch> = children.iter().filter(|x| x.borrow().elem.selected).collect();
+		let children: Vec<&RcPath> = children.iter().filter(|x| x.borrow().selected).collect();
 		for (i, child) in children.iter().enumerate() {
 			let (pre, addon) = if i == children.len() - 1 {
 				("    ", "└── ")
@@ -225,41 +254,40 @@ fn _tree_string(root: &PathBranch, lines: &mut Vec<String>, prefix: &str) {
 				("│   ", "├── ")
 			};
 
-			lines.push(prefix.to_owned() + addon + child.borrow().elem.basename());
+			lines.push(prefix.to_owned() + addon + child.basename());
 			if child.borrow().children.is_some() {
 				_tree_string(child, lines, &(prefix.to_owned() + pre));
 			}
 		}
 	} else {
-		lines.push(prefix.to_owned() + root.borrow().elem.basename());
+		lines.push(prefix.to_owned() + root.basename());
 	}
 }
 
 /// Create a string representation of the tree from root directory `root`.
 /// We can preallocate the exact capacity by knowing the number of paths
 /// we are constructing for.
-pub fn tree_string(root: &PathBranch, len: usize) -> Vec<String> {
+pub fn tree_string(root: &RcPath, len: usize) -> Vec<String> {
 	let mut lines = Vec::with_capacity(len);
 	_tree_string(root, &mut lines, "");
 	lines
 }
 
-#[macro_export]
-macro_rules! paths {
-	( $( $x:literal ),* ) => {
-		{
-			let mut temp = Vec::new();
-			$(
-				temp.push(Path::from($x));
-			)*
-			temp
-		}
-	};
-}
-
 #[cfg(test)]
 mod test {
 	use super::*;
+
+	macro_rules! paths {
+		( $( $x:literal ),* ) => {
+			{
+				let mut temp = Vec::new();
+				$(
+					temp.push(Path::from($x));
+				)*
+				temp
+			}
+		};
+	}
 
 	#[test]
 	fn sorting_is_correct() {
@@ -277,14 +305,32 @@ mod test {
 		path1 = Path::from("here/is/a/fath.c");
 		path2 = Path::from("here/is/a/path.c");
 		assert!(path1 < path2);
-	}
 
-	#[test]
-	fn sorting_is_correct_2() {
 		let mut paths = paths!["src", "tmp", "src/main.rs"];
 		paths.sort();
 		let expected = paths!["src", "src/main.rs", "tmp"];
 		assert_eq!(paths, expected);
+	}
+
+	#[test]
+	fn len_correct() {
+		let s = "here/is/a/path.c";
+		let path = Path::from(s);
+		assert_eq!(path.len(), 4);
+	}
+
+	#[test]
+	fn basename_correct() {
+		let s = "here/is/a/path.c";
+		let path = Path::from(s);
+		assert_eq!(path.basename(), "path.c");
+	}
+
+	#[test]
+	fn joined_joins_components() {
+		let s = "here/is/a/path.c";
+		let path = Path::from(s);
+		assert_eq!(path.joined(), s);
 	}
 
 	#[test]
@@ -303,20 +349,20 @@ mod test {
 	fn is_child_of_correctness() {
 		let mut p1 = Path::from("A");
 		let mut p2 = Path::from("B");
-		assert!(!is_child_of(&p1, &p2));
+		assert!(!p1.is_child_of(&p2));
 
 		p1 = Path::from("src/bayes");
 		p2 = Path::from("src/bayes/blend.c");
-		assert!(!is_child_of(&p1, &p2));
+		assert!(!p1.is_child_of(&p2));
 
 		p1 = Path::from("src/bayes");
 		p2 = Path::from("src/bayes/blend.c");
-		assert!(is_child_of(&p2, &p1));
+		assert!(p2.is_child_of(&p1));
 	}
 
 	/// Determine whether two trees are equal by recursing into all branches
-	fn trees_equal(a: &PathBranch, b: &PathBranch) -> bool {
-		if a.borrow().elem != b.borrow().elem {
+	fn trees_equal(a: &RcPath, b: &RcPath) -> bool {
+		if a != b {
 			return false;
 		}
 
@@ -338,7 +384,7 @@ mod test {
 		true
 	}
 
-	fn create_test_paths() -> Vec<Path> {
+	fn create_test_paths() -> Vec<RcPath> {
 		paths![
 			".",
 			"./A",
@@ -354,58 +400,34 @@ mod test {
 		]
 	}
 
-	fn create_test_tree(paths: &Vec<Path>) -> PathBranch {
-		let expected: Vec<PathBranch> = paths.iter().map(tree::Branch::new).collect();
-		let root = Rc::clone(&expected[0]);
-		root.add_child(&expected[1]);
-		root.add_child(&expected[2]);
-		root.add_child(&expected[3]);
-		&expected[3].add_child(&expected[4]);
-		&expected[4].add_child(&expected[5]);
-		&expected[4].add_child(&expected[6]);
-		&expected[3].add_child(&expected[7]);
-		&expected[7].add_child(&expected[8]);
-		&expected[7].add_child(&expected[9]);
-		root.add_child(&expected[10]);
+	fn create_test_tree(paths: &Vec<RcPath>) -> RcPath {
+		let root = Rc::clone(&paths[0]);
+		root.add_child(&paths[1]);
+		root.add_child(&paths[2]);
+		root.add_child(&paths[3]);
+		&paths[3].add_child(&paths[4]);
+		&paths[4].add_child(&paths[5]);
+		&paths[4].add_child(&paths[6]);
+		&paths[3].add_child(&paths[7]);
+		&paths[7].add_child(&paths[8]);
+		&paths[7].add_child(&paths[9]);
+		root.add_child(&paths[10]);
 		root
 	}
 
 	#[test]
 	fn create_tree_correct() {
 		let paths = create_test_paths();
-
-		let branches: Vec<PathBranch> = paths.iter().map(tree::Branch::new).collect();
-		let root = create_tree(&branches);
-		println!("{:?}", root); // TODO: Use log
-
+		let root = create_tree(&paths);
 		let root2 = create_test_tree(&paths);
 		assert!(trees_equal(&root, &root2));
-	}
-
-	#[test]
-	fn tree_string_test_with_unselected() {
-		let mut paths = create_test_paths();
-		for p in paths.iter_mut() {
-			p.selected = true;
-		}
-		paths[4].selected = false;
-		let root = create_test_tree(&paths);
-		let lines = tree_string(&root, paths.len());
-		let expected = "├── A
-├── B
-├── src
-│   └── cakes
-│       ├── a.c
-│       └── b.c
-└── x.txt";
-		assert_eq!(lines.join("\n"), expected);
 	}
 
 	#[test]
 	fn tree_string_test() {
 		let mut paths = create_test_paths();
 		for p in paths.iter_mut() {
-			p.selected = true;
+			p.borrow_mut().selected = true;
 		}
 		let root = create_test_tree(&paths);
 		let lines = tree_string(&root, paths.len());
@@ -415,6 +437,18 @@ mod test {
 │   ├── bayes
 │   │   ├── blend.c
 │   │   └── rand.c
+│   └── cakes
+│       ├── a.c
+│       └── b.c
+└── x.txt";
+		assert_eq!(lines.join("\n"), expected);
+
+		// Deselect `./src/bayes` and print again
+		paths[4].borrow_mut().selected = false;
+		let lines = tree_string(&root, paths.len());
+		let expected = "├── A
+├── B
+├── src
 │   └── cakes
 │       ├── a.c
 │       └── b.c
